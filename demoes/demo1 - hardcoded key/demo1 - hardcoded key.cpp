4dc4289dd1e23560cf1657b56b6de1dc9274eae5
iagox86 <ron-git@skullsecurity.org>
2019-11-18 09:02:25 -0800

    Make the key in demo1 a bit more obvious

--- demoes/demo1 - hardcoded key/demo1 - hardcoded key.cpp
+++ demoes/demo1 - hardcoded key/demo1 - hardcoded key.cpp
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* This is BAD! */
+#define KEY "\xf0\xa9\xaa\xf1\x2a\xdc\x69\xc6\x50\x9e\x9a\xbe\xf2\x76\x8c\xde"
+
 void do_aes(char *key, char *data) {
 	// TODO (if this was real)
 }
 
 void do_encrypt(char *text) {
-	do_aes("\xf0\xa9\xaa\xf1\x2a\xdc\x69\xc6\x50\x9e\x9a\xbe\xf2\x76\x8c\xde", text);
+	do_aes(KEY, text);
 }
 
 int main(int argc, char *argv[]) {
