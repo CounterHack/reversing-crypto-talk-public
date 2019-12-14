8654f2520424078ab379119720b145960e6ea9b6
iagox86 <ron-git@skullsecurity.org>
2019-12-14 13:19:09 -0800

    Some small cleanup

--- demoes/demo5 - recognizing modes/demo5 - recognizing modes.rb
+++ demoes/demo5 - recognizing modes/demo5 - recognizing modes.rb
@@ -1,17 +1,16 @@
 require 'openssl'
-
 def ecb(data)
   c = OpenSSL::Cipher::DES.new('ECB')
   c.encrypt
   c.key = "AAAAAAAA"
-  return (c.update(data) + c.final())
+  return (c.update(data) + c.final()).unpack('H*').pop.scan(/(.{16})/).join(' ')
 end
 
 def cbc(data)
   c = OpenSSL::Cipher::DES.new('CBC')
   c.encrypt
   c.key = "AAAAAAAA"
-  return (c.update(data) + c.final())
+  return (c.update(data) + c.final()).unpack('H*').pop.scan(/(.{16})/).join(' ')
 end
 
 puts("ECB")
@@ -19,7 +18,7 @@ BASE = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
 0.upto(47) do |i|
   test = BASE.clone
   test[i] = 'B'
-  puts("Changing byte %02d => #{ecb(test).unpack('H*').pop}" % i)
+  puts("Changing byte %02d => #{ecb(test)}" % i)
 end
 puts()
 
@@ -27,7 +26,7 @@ puts("CBC")
 0.upto(47) do |i|
   test = BASE.clone
   test[i] = 'B'
-  puts("Changing byte %02d => #{cbc(test).unpack('H*').pop}" % i)
+  puts("Changing byte %02d => #{cbc(test)}" % i)
 end
 puts()
 
